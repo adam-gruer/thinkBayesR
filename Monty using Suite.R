3e85239a3455731b8c58137bf957d12deefebe5b
Adam Gruer <abbot.grover@gmail.com>
2017-09-29 17:46:02 +1000

    Monty inheriting from Suite

---
+++ Monty using Suite.R
@@ -0,0 +1,18 @@
+source("Suite.R")
+
+Monty <- R6Class("Monty",
+                 inherit = Suite,
+                 public = list(
+                   Likelihood = function(data,hypo) {
+                     if(hypo == data) {
+                       0
+                     } else if (hypo == "A"){
+                       0.5
+                     } else {
+                       1
+                     }
+                   }
+                 )) 
+suite <-  Monty$new(LETTERS[1:3])
+suite$Update('B')
+suite
