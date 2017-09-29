cb4cdad036adbb493eff7c45312bef8c70397a4f
Adam Gruer <abbot.grover@gmail.com>
2017-09-29 17:29:36 +1000

    Suite abstract class

---
+++ Suite.R
@@ -0,0 +1,22 @@
+source("Pmf.R")
+
+Suite <- R6Class("Suite",
+                 inherit = Pmf,
+                 public = list(
+                   Update = function(data){
+                     
+                     like <- self$Likelihood(data, hypo)
+                     self$Mult(hypo, like)
+                     self$Normalize()
+                     
+                   },
+                   print = function(){
+                     for (item in self$Items()){
+                       cat(item[[1]],item[[2]],"\n")
+                     }},
+                    Likelihood  = function(){
+                      stop("A Likelihood method needs to be implement for this subclass",
+                           call. = FALSE)
+                    }
+                   
+                 ))
