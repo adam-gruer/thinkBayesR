3d356d7e58a73be7e51ac28a1b42126b9ad24d67
Adam Gruer <abbot.grover@gmail.com>
2017-09-29 20:55:31 +1000

    COnvert vectors and lists to hashes
    No problem here with hashes.  DO need to watch out for subsetting with [[ to return the item in the hash separate from the hash structure - like with lists

--- M_and_M.R
+++ M_and_M.R
@@ -3,36 +3,34 @@ source("Suite.R")
 M_and_M <- R6Class("M_and_M",
                    inherit = Suite,
                    private = list(
-                     mix94 = c(brown = 30,
+                     mix94 = hash(brown = 30,
                                   yellow=20,
                                   red=20,
                                   green=10,
                                   orange=10,
                                   tan=10),
                      
-                     mix96 = c(blue=24,
+                     mix96 = hash(blue=24,
                                        green=20,
                                        orange=16,
                                        yellow=14,
                                        red=13,
                                        brown=13),
                      
-                     hypoA = function(){list(bag1 = private$mix94,
+                     hypoA = function(){hash(bag1 = private$mix94,
                                           bag2 = private$mix96)},
                      
-                     hypoB = function(){list(bag1 = private$mix96,
+                     hypoB = function(){hash(bag1 = private$mix96,
                                           bag2 = private$mix94)},
                      
-                     hypotheses = function(){list(A=private$hypoA(),
+                     hypotheses = function(){hash(A=private$hypoA(),
                                                B=private$hypoB())
                        }),
-                     public = list(
+                   public = list(
                      Likelihood = function(data, hypo){
                          bag <-  data[1]
                          color <- data[2]
-                         mix <- private$hypotheses()[[hypo]][[bag]]
-                         mix[[color]]
-                         
+                         private$hypotheses()[[hypo]][[bag]][[color]]
                      }
                    ))
 
@@ -40,5 +38,4 @@ suite = M_and_M$new(LETTERS[1:2])
 
 suite$Update(c("bag1", "yellow"))
 suite$Update(c("bag2", "green"))
-
-suite$
+suite
