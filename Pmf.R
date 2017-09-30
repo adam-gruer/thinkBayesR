c255a320c76357744aed5119621f13148b84644a
Adam Gruer <abbot.grover@gmail.com>
2017-09-30 11:45:25 +1000

    add Mean method to Pmf class

--- Pmf.R
+++ Pmf.R
@@ -5,6 +5,17 @@ Pmf <- R6Class("Pmf",
                public = list(
                  Prob = function(x, default=0){
                    ifelse(has.key(x,self$d), self$d[[x]],default)
+                 },
+                 Mean = function(){
+                   items <- self$Items()
+                   l <- length(items)
+                   x <- vector("numeric",l)
+                   p <- x
+                   for(i in 1:l){
+                     x[i] <- items[[i]][[1]]
+                     p[i] <- items[[i]][[2]]
+                   }
+                   sum(p * x)
                  }
                ))
 
