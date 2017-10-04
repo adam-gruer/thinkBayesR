e8021edda10da427497b489bacc5f904c3715b99
Adam Gruer <abbot.grover@gmail.com>
2017-10-04 22:13:53 +1100

    Dict initialis, check for duplicates

--- R/Dict.R
+++ R/Dict.R
@@ -1,21 +1,27 @@
 Dict <- R6::R6Class("Dict",
                     public = list(
-                      initialize = function(values = numeric(), probs = numeric){
-                        l = length(values)
-                        private$checkDuplicates(values)
-                        
+                      initialize = function(values = numeric(), probs = numeric()){
+                        private$l <-  length(values)
+                        private$check_duplicates(values)
+                        private$dict <- private$add_items(values, probs)
                         
                         
                       }
                     ),
                      private = list(
-                      dict = NULL,
-                      check_duplicates = function(values){
-                        if(anyDuplicated(values)){
+                      dict = list(values = numeric(),
+                                  probs = numeric()
+                                  ),
+                      l = NULL,
+                      check_duplicates = function(values,...){
+                        if(anyDuplicated(values,nmax = private$l,...)){
                           stop("No duplicate values are allowed", call. = FALSE)
+                          
+                          invisible(values)
                         }
-                      }
-                      
+                      },
+                      add_items =  function(values, probs){}
+                      check_duplicates(c(private$dict$values))
                       
                     )
                     )
