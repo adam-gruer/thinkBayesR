b4bca1f6d15fd0a9441c5e6d458a357f989ba5fd
Adam Gruer <abbot.grover@gmail.com>
2017-10-04 17:56:24 +1100

    Start of a specialized Dict class

---
+++ Dict.R
@@ -0,0 +1,16 @@
+Dict <- R6::R6Class("Dict",
+                    public = list(
+                      initialize = function(values = numeric(), probs = numeric){
+                        l = length(values)
+                        if (any)
+                        
+                        
+                        
+                      }
+                    ),
+                     private = list(
+                      dict = NULL,
+                      
+                      ,
+                    )
+                    )
--- matrix_operations.R
+++ matrix_operations.R
@@ -1,16 +1,16 @@
-hypos <- 1:1000
-pmf <- matrix(c(hypos,hypos),
+hypos <- 1:1e3
+pmf <- matrix(c(hypos,rep_len(1,length(hypos))),
               ncol = 2,
               dimnames = list(NULL, c("values","probabilities")))
 
 
 #acess a column by name
 pmf[,"probabilities"]
-pmf["values",]
+pmf[,"values"]
 
 # filter rows by value
 pmf[pmf[,"values"] == 34, , drop = FALSE] # drop = FALSE result is a matrix, TRUE (default) - result is vector
-
+.subset2(pmf, select = "probabilities")
 (value_only <- pmf[pmf[,"values"] == 34, "values"])
 (value_only <- pmf[pmf[,"values"] == 34, "probabilities"])
 
