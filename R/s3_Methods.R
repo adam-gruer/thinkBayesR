c302849d2eb5f7d1fe14919294dde3c86bb3ccf0
Adam Gruer <adamgruer@icloud.com>
2017-10-10 13:39:11 +1100

    add length method for classs DictWrapper

--- R/s3_Methods.R
+++ R/s3_Methods.R
@@ -20,6 +20,9 @@ length.Dict <- function(x){
   length(x$values)
 }
 
+length.DictWrapper <- function(x){
+  length(x$Values())
+}
 
 #Create generic for Percentile
 Percentile <- function(x, percentage,...){
@@ -29,16 +32,5 @@ Percentile <- function(x, percentage,...){
 #Method where x has the class Pmf
 Percentile.Pmf <-  function(x, percentage,...){
   p <- percentage / 100.0
-  total = 0
-  probs <- x$Probs()
-
-  
-  for (i in seq_along(probs)){
-    total <- total + probs[i]
-  if (total >= p){
-    val <- x$Values()[i]
-    break
-  }
-  }
-  val
+  suite$Values()[cumsum(suite$Probs()) >= p][1]
 }
